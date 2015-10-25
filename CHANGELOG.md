40b30eb8073f2d37f23adcaebc5ded12bf595b76
Uri Shaked <uri@salsa4fun.co.il>
2015-10-26 00:30:33 +0200

    chore(*): release 0.0.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 0.0.4 - 2015-10-25
+- Add ES5 transpiled version and typescript definitions (.d.ts) file to the published npm package
+
 ## 0.0.3 - 2015-10-22
 - Align with angular-2.0.0-alpha.44
 
--- TimeAgoPipe.ts
+++ TimeAgoPipe.ts
@@ -1,4 +1,4 @@
-/* angular2-moment / v0.0.3 / (c) 2015 Uri Shaked / MIT Licence */
+/* angular2-moment / v0.0.4 / (c) 2015 Uri Shaked / MIT Licence */
 
 /// <reference path="node_modules/angular2/bundles/typings/angular2/angular2.d.ts" />
 /// <reference path="typings/moment/moment.d.ts" />
