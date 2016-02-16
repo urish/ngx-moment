2dcd6135ae5c0a72a5311a73a17369621f1b53d0
Uri Shaked <uri@salsa4fun.co.il>
2016-02-16 16:15:30 +0200

    test(TimeAgoPipe): additional unit tests

--- TimeAgoPipe.spec.ts
+++ TimeAgoPipe.spec.ts
@@ -5,9 +5,33 @@ import {TimeAgoPipe} from './TimeAgoPipe';
 
 describe('TimeAgoPipe', () => {
   describe('#transform', () => {
+    afterEach(function() {
+      jasmine.clock().uninstall();
+    });
+
     it('should transform the current date to "a few seconds ago"', () => {
       const pipe = new TimeAgoPipe(null);
       expect(pipe.transform(new Date())).toBe('a few seconds ago');
+    });
+
+    it('should automatically update the text as time passes', () => {
+      const changeDetectorMock = jasmine.createSpyObj('ChangeDetectorRef', ['markForCheck']);
+      const pipe = new TimeAgoPipe(changeDetectorMock);
+      jasmine.clock().install();
+      expect(pipe.transform(new Date())).toBe('a few seconds ago');
+      expect(changeDetectorMock.markForCheck).not.toHaveBeenCalled();
+      jasmine.clock().tick(60000);
+      expect(changeDetectorMock.markForCheck).toHaveBeenCalled();
+    });
+
+    it('should remove all timer when destroyed', () => {
+      const changeDetectorMock = jasmine.createSpyObj('ChangeDetectorRef', ['markForCheck']);
+      const pipe = new TimeAgoPipe(changeDetectorMock);
+      jasmine.clock().install();
+      expect(pipe.transform(new Date())).toBe('a few seconds ago');
+      pipe.ngOnDestroy();
+      jasmine.clock().tick(60000);
+      expect(changeDetectorMock.markForCheck).not.toHaveBeenCalled();
     });
   });
 });
