87bc1497ebd835b1dfe854c9037981b7c76a9b53
Tiago Roldão <focus5.6@gmail.com>
2016-09-22 15:38:58 +0100

    Removed impure flags from pure Pipes

--- src/DifferencePipe.ts
+++ src/DifferencePipe.ts
@@ -6,7 +6,7 @@ import * as moment from 'moment';
 // under systemjs, moment is actually exported as the default export, so we account for that
 const momentConstructor: (value?: any) => moment.Moment = (<any>moment).default || moment;
 
-@Pipe({ name: 'amDifference', pure: false })
+@Pipe({ name: 'amDifference' })
 export class DifferencePipe implements PipeTransform {
   transform(value: Date | moment.Moment,
             otherValue: Date | moment.Moment,
