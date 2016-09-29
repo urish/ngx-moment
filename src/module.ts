67aaabdd5d92ebfabefeaf1e8704331942d372f9
Dustin <dchilders@protonmail.com>
2016-09-28 22:28:36 -0500

    Added AddPipe

--- src/module.ts
+++ src/module.ts
@@ -6,9 +6,10 @@ import { FromUnixPipe } from './FromUnixPipe';
 import { TimeAgoPipe } from './TimeAgoPipe';
 import { DifferencePipe } from './DifferencePipe';
 import { SubtractPipe } from './SubtractPipe';
+import { AddPipe } from './MAddPipe';
 
 const ANGULAR_MOMENT_PIPES = [CalendarPipe, DateFormatPipe, DurationPipe, FromUnixPipe, TimeAgoPipe, DifferencePipe,
-                              SubtractPipe];
+                              SubtractPipe, AddPipe];
 
 @NgModule({
   declarations: ANGULAR_MOMENT_PIPES,
