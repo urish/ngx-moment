2f9d90352081b7fe18824d302d552f06d79627ea
Dustin <dchilders@protonmail.com>
2016-09-29 06:31:22 -0500

    AddPipe: Fixed issue with file name

---
+++ src/AddPipe.spec.ts
@@ -0,0 +1,31 @@
+import 'es6-shim';
+import 'reflect-metadata';
+import * as moment from 'moment';
+import { AddPipe } from './AddPipe';
+
+describe('AddPipe', () => {
+  var pipe: AddPipe;
+
+  beforeEach(() => pipe = new AddPipe());
+
+  describe('#transform', () => {
+    it('should throw when provided no arguments', () => {
+      expect(() => pipe.transform(128)).toThrow(new Error('AddPipe: missing required arguments'));
+    });
+
+    it('should add two hours', () => {
+      const result = pipe.transform(moment('2016-01-24 15:00:00'), '2', 'hours');
+      expect(moment(result).format('YYYY-MM-DD HH:mm:ss')).toBe('2016-01-24 17:00:00');
+    });
+
+    it('should add two days', () => {
+      const result = pipe.transform(moment('2016-01-24 15:00:00'), '2', 'days');
+      expect(moment(result).format('YYYY-MM-DD HH:mm:ss')).toBe('2016-01-26 15:00:00');
+    });
+
+    it('should add two years', () => {
+      const result = pipe.transform(moment('2016-01-24 15:00:00'), '2', 'years');
+      expect(moment(result).format('YYYY-MM-DD HH:mm:ss')).toBe('2018-01-24 15:00:00');
+    });
+  });
+});
