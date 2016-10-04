10ea5faf928ef925734d1ee1ec3c62748e67369e
Saurabh Gupta <sg1705@gmail.com>
2016-10-03 20:53:54 -0400

    specifying modules as per rollup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export * from './module';
-export * from './CalendarPipe';
-export * from './DateFormatPipe';
-export * from './DurationPipe';
-export * from './FromUnixPipe';
-export * from './TimeAgoPipe';
-export * from './DifferencePipe';
+export { MomentModule } from './module';
+export { CalendarPipe } from './CalendarPipe';
+export { DateFormatPipe } from './DateFormatPipe';
+export { DurationPipe } from './DurationPipe';
+export { FromUnixPipe } from './FromUnixPipe';
+export { TimeAgoPipe } from './TimeAgoPipe';
+export { DifferencePipe } from './DifferencePipe';
