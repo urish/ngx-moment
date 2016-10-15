81752fe3e08a17c1e62bb78c9bfc2e744c04f158
Yannick Koehler <yannick@koehler.name>
2016-10-15 15:18:51 -0400

    Fix urish/angular2-moment#99 - Added supports for amCalendar custom formats

--- src/calendar.pipe.spec.ts
+++ src/calendar.pipe.spec.ts
@@ -2,7 +2,7 @@ import 'es6-shim';
 import 'reflect-metadata';
 import * as moment from 'moment';
 import {NgZone} from '@angular/core';
-import {CalendarPipe} from './calendar.pipe';
+import {CalendarPipe, CalendarFormats} from './calendar.pipe';
 
 class NgZoneMock {
   runOutsideAngular (fn: Function) {
@@ -15,9 +15,15 @@ class NgZoneMock {
 
 describe('CalendarPipe', () => {
   describe('#transform', () => {
-    const pipe = new CalendarPipe(null, new NgZoneMock() as NgZone);
     it('should transform the start of the current day to "Today at 12:00 AM"', () => {
+      const pipe = new CalendarPipe(null, new NgZoneMock() as NgZone);
       expect(pipe.transform(moment().startOf('day'))).toBe('Today at 12:00 AM');
+    });
+    it('should transform the start of the current day to "Today"', () => {
+      const pipe = new CalendarPipe(null, new NgZoneMock() as NgZone);
+      let formats = new CalendarFormats();
+      formats.sameDay = '[Today]';
+      expect(pipe.transform(moment().startOf('day'), null, formats)).toBe('Today');
     });
   });
 });
