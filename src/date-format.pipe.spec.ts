0e197f5b942b1e0016e56187bbf843c833ef76b1
Austin McDaniel <amcdaniel2@gmail.com>
2016-10-24 09:25:15 -0400

    (bug): add support for undefined date (#41)

--- src/date-format.pipe.spec.ts
+++ src/date-format.pipe.spec.ts
@@ -10,5 +10,18 @@ describe('DateFormatPipe', () => {
       const result = pipe.transform(moment('2016-01-24 01:23:45'), 'MMMM Do YYYY, h:mm:ss a');
       expect(result).toBe('January 24th 2016, 1:23:45 am');
     });
+
+    it('should not format empty dates', () => {
+      const pipe = new DateFormatPipe();
+
+      const result1 = pipe.transform('', 'MMMM Do YYYY, h:mm:ss a');
+      expect(result1).toBe('');
+
+      const result2 = pipe.transform(null);
+      expect(result2).toBe('');
+
+      const result3 = pipe.transform(undefined);
+      expect(result3).toBe('');
+    });
   });
 });
