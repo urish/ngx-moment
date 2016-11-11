432da35871366884963e0c37cd920b01f3f62222
Uri Shaked <uri@salsa4fun.co.il>
2016-11-11 10:45:55 -0800

    fix(*): align with moment 2.16.x typings
    fix #112

--- src/difference.pipe.ts
+++ src/difference.pipe.ts
@@ -10,7 +10,7 @@ const momentConstructor: (value?: any) => moment.Moment = (<any>moment).default 
 export class DifferencePipe implements PipeTransform {
   transform(value: Date | moment.Moment,
             otherValue: Date | moment.Moment,
-            unit?: moment.UnitOfTime,
+            unit?: moment.unitOfTime.Diff,
             precision?: boolean): number {
 
     let date = momentConstructor(value);
