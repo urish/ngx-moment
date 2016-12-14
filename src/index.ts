ef840b042ba890a789cb8bd8c47e2d4a2f3fddd2
Dustin <dchilders@protonmail.com>
2016-12-13 18:45:23 -0600

    sync changes

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { CalendarPipe } from './calendar.pipe';
+export { DateFormatPipe } from './date-format.pipe';
+export { DifferencePipe } from './difference.pipe';
+export { DurationPipe } from './duration.pipe';
+export { FromUnixPipe } from './from-unix.pipe';
+export { MomentModule } from './moment.module';
+export { TimeAgoPipe } from './time-ago.pipe';
