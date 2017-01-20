e1f7681fbabbcdc0cd5341258c09fd2328affea9
Brandon Bodnar <bbodnar@asymmetrik.com>
2017-01-20 16:09:51 -0500

    Add UtcPipe to enable UTC mode in pipe chain.

---
+++ src/utc.pipe.spec.ts
@@ -0,0 +1,61 @@
+import 'es6-shim';
+import 'reflect-metadata';
+import * as moment from 'moment';
+import { DateFormatPipe } from './date-format.pipe';
+import { UtcPipe } from './utc.pipe';
+
+describe('UtcPipe', () => {
+
+  describe('#transform', () => {
+
+    let utcDatePipe: UtcPipe;
+
+    beforeEach(() => {
+      utcDatePipe = new UtcPipe();
+    });
+
+    it('should output an invalid momemt object for a null input', () => {
+      const utcDate = utcDatePipe.transform(null);
+      expect(utcDate).toEqual(jasmine.any(moment));
+      expect(utcDate.isValid()).toBe(false);
+    });
+
+    it('should output a moment object for a moment input', () => {
+      const momentDate = moment();
+      const utcDate = utcDatePipe.transform(momentDate);
+      expect(utcDate).toEqual(jasmine.any(moment));
+      expect(utcDate.isValid()).toBe(true);
+    });
+
+    it('should output a moment object for a date input', () => {
+      const date = new Date();
+      const utcDate = utcDatePipe.transform(date);
+      expect(utcDate).toEqual(jasmine.any(moment));
+      expect(utcDate.isValid()).toBe(true);
+    });
+
+    it('should output a moment object for a string date', () => {
+      const dateString = '2016-01-01';
+      const utcDate = utcDatePipe.transform(dateString);
+      expect(utcDate).toEqual(jasmine.any(moment));
+      expect(utcDate.isValid()).toBe(true);
+    });
+
+    it('should output a moment object for a timestamp', () => {
+      const timestamp: number = Date.now();
+      const utcDate = utcDatePipe.transform(timestamp);
+      expect(utcDate).toEqual(jasmine.any(moment));
+      expect(utcDate.isValid()).toBe(true);
+    });
+
+    it('should be pipeable to amDateFormat', () => {
+      const amDateFormat = new DateFormatPipe();
+      const datetimeString = '2015-12-31T23:00:00.000-01:00';
+      const momentFormatString = 'YYYY-MM-DD';
+      const utcOutput = utcDatePipe.transform(datetimeString);
+      expect(amDateFormat.transform(utcOutput, momentFormatString)).toEqual('2016-01-01');
+    });
+
+  });
+
+});
