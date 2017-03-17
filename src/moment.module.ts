93aae516836dcc092182bc061960348f0c478ef0
David Atencia <david.atencia@gmail.com>
2017-03-17 17:03:05 +0100

    fix: add missing `add` and `subtract` pipes
    they were missing in moment.module.ts

--- src/moment.module.ts
+++ src/moment.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 
+import { AddPipe } from './add.pipe';
 import { CalendarPipe } from './calendar.pipe';
 import { DateFormatPipe } from './date-format.pipe';
 import { DifferencePipe } from './difference.pipe';
 import { DurationPipe } from './duration.pipe';
 import { FromUnixPipe } from './from-unix.pipe';
+import { SubtractPipe } from './subtract.pipe';
 import { TimeAgoPipe } from './time-ago.pipe';
 import { UtcPipe } from './utc.pipe';
 
-const ANGULAR_MOMENT_PIPES = [CalendarPipe, DateFormatPipe, DifferencePipe, DurationPipe, FromUnixPipe, TimeAgoPipe,
-   UtcPipe];
+const ANGULAR_MOMENT_PIPES = [
+  AddPipe,
+  CalendarPipe,
+  DateFormatPipe,
+  DifferencePipe,
+  DurationPipe,
+  FromUnixPipe,
+  SubtractPipe,
+  TimeAgoPipe,
+  UtcPipe
+];
 
 @NgModule({
   declarations: ANGULAR_MOMENT_PIPES,
