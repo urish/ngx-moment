47dceaa3645123b5256af497966cbb5c1b1fc6b9
Uri Shaked <uri@salsa4fun.co.il>
2017-03-18 02:25:59 +0200

    fix: add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+export { AddPipe } from './add.pipe';
 export { CalendarPipe } from './calendar.pipe';
 export { DateFormatPipe } from './date-format.pipe';
 export { DifferencePipe } from './difference.pipe';
 export { DurationPipe } from './duration.pipe';
 export { FromUnixPipe } from './from-unix.pipe';
 export { MomentModule } from './moment.module';
+export { SubtractPipe } from './subtract.pipe';
 export { TimeAgoPipe } from './time-ago.pipe';
 export { UtcPipe } from './utc.pipe';
