253a041d9514a196d4c1bdd3cc7fb4140bb1df1b
Connor Lewis <cmlewis95@gmail.com>
2017-08-15 19:15:15 -0400

    Adds `amFromUtc` pipe (#163)
    * Parse input as UTC formatted date using moment.utc()

---
+++ src/from-utc.pipe.ts
@@ -0,0 +1,11 @@
+/* angular2-moment (c) 2015, 2016 Uri Shaked / MIT Licence */
+
+import {Pipe, PipeTransform} from '@angular/core';
+import * as moment from 'moment';
+
+@Pipe({ name: 'amFromUtc' })
+export class FromUtcPipe implements PipeTransform {
+  transform(value: any, ...args: string[]): any {
+    return moment.utc(value);
+  }
+}
