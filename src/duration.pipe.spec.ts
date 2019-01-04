7f6a8c4079ecd39512487b832b240309213b9f77
StickNitro <stevensn99@googlemail.com>
2019-01-04 12:07:04 +0000

    feat: ability to specify relativeTimeThreshold for issue #202
    add `forRoot` to `MomentModule` to allow passing of `NgxMomentOptions`, can be easily extended to support other options (both global and local to pipes)
    added optional parameter to constructor of DurationPipe to provide options and allows pipe to specify `relativeTimeThreshold` units
    added tests to support

--- src/duration.pipe.spec.ts
+++ src/duration.pipe.spec.ts
@@ -1,4 +1,5 @@
-import {DurationPipe} from './duration.pipe';
+import { DurationPipe } from './duration.pipe';
+import { NgxMomentOptions } from './moment-options';
 
 describe('DurationPipe', () => {
   let pipe: DurationPipe;
@@ -16,6 +17,24 @@ describe('DurationPipe', () => {
       expect(pipe.transform(365, 'seconds')).toEqual('6 minutes');
       expect(pipe.transform(365, 'days')).toEqual('a year');
       expect(pipe.transform(86400, 'seconds')).toEqual('a day');
+    });
+
+    it(`should convert '50 minutes' to 'an hour' with default 'relativeTimeThreshold'`, () => {
+      expect(pipe.transform(50, 'minutes')).toEqual('an hour');
+    });
+  });
+
+  describe('ctor with NgxMomentOptions', () => {
+    const momentOptions: NgxMomentOptions = {
+      relativeTimeThresholdOptions: {
+        'm': 59
+      }
+    };
+
+    beforeEach(() => pipe = new DurationPipe(momentOptions));
+
+    it(`should convert '50 minutes' to '50 minutes' when relativeTimeThreshold for 'm' unit is set to 59`, () => {
+      expect(pipe.transform(50, 'minutes')).toEqual('50 minutes');
     });
   });
 });
--- src/duration.pipe.ts
+++ src/duration.pipe.ts
@@ -1,12 +1,36 @@
-import {Pipe, PipeTransform} from '@angular/core';
 import * as moment from 'moment';
+
+import { Inject, Optional, Pipe, PipeTransform } from '@angular/core';
+import { NGX_MOMENT_OPTIONS, NgxMomentOptions } from './moment-options';
 
 @Pipe({ name: 'amDuration' })
 export class DurationPipe implements PipeTransform {
+
+  allowedUnits: Array<string> = ['ss', 's', 'm', 'h', 'd', 'M'];
+
+  constructor(@Optional() @Inject(NGX_MOMENT_OPTIONS) momentOptions?: NgxMomentOptions) {
+    this._applyOptions(momentOptions);
+  }
+
   transform(value: any, ...args: string[]): string {
     if (typeof args === 'undefined' || args.length !== 1) {
       throw new Error('DurationPipe: missing required time unit argument');
     }
     return moment.duration(value, args[0] as moment.unitOfTime.DurationConstructor).humanize();
   }
+
+  private _applyOptions(momentOptions: NgxMomentOptions): void {
+    if (!!!momentOptions) {
+      return;
+    }
+
+    if (!!momentOptions.relativeTimeThresholdOptions) {
+      const units: Array<string> = Object.keys(momentOptions.relativeTimeThresholdOptions);
+      const filteredUnits: Array<string> = units.filter(unit => this.allowedUnits.indexOf(unit) !== -1);
+      filteredUnits.forEach(unit => {
+        moment.relativeTimeThreshold(unit, momentOptions.relativeTimeThresholdOptions[unit]);
+      });
+    }
+  }
+
 }
